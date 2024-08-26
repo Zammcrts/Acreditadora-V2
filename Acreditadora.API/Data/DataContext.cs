28c9826cc3ac0768ecdc22723c7e1b3710ad2f62
Zammcrts <samcortes480@gmail.com>
2024-08-26 09:36:58 -0600

    contexto de datos

---
+++ Acreditadora.API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Acreditadora.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Acreditadora.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DbSet<University> Universities { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) //solo puede existir una
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<University>().HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
---
+++ Acreditadora.Shared/Entities/University.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acreditadora.Shared.Entities
+{
+    public class University
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El campo {0} es obligatorio")] //obligatorio pertenece al public string Name
+        [Display(Name = "Universidad")]
+        [MaxLength(100, ErrorMessage ="El campo {0} no puede tener mas de {1} caracteres")]
+        public string Name { get; set; } = null!; //no acepta valores nulos
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] //obligatorio pertenece al public string Name
+        [Display(Name = "Sitio Web")]
+        [MaxLength(200, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string Url { get; set; } =null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] //obligatorio pertenece al public string Name
+        [Display(Name = "Telefono")]
+        [MaxLength(10, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string PhoneNumber { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] //obligatorio pertenece al public string Name
+        [Display(Name = "Direccion")]
+        [MaxLength(200, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string Address { get; set; } = null!;
+        
+    }
+}
