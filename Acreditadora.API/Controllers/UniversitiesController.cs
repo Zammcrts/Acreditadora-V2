bd5eb34d1f383c3b2da865c8dd3f322dece9847b
Zammcrts <samcortes480@gmail.com>
2024-09-08 11:28:35 -0600

    entidades y datacontext

---
+++ Acreditadora.API/Controllers/UniversitiesController.cs
@@ -0,0 +1,19 @@
+﻿using Acreditadora.API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Acreditadora.API.Controllers
+{
+
+    //directivas
+    [ApiController]
+    [Route("/api/universities")]
+    public class UniversitiesController:ControllerBase
+    {
+        private readonly DataContext dataContext;
+
+        public UniversitiesController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+    }
+}
